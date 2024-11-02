6fa7fd6e63ed65fe2f641bed3257c7e9621fa5ec
Minh Nguyên <3langn@gmail.com>
2024-11-02 11:13:48 +0700

    Refactor generateHash function to use a lower cost factor for bcrypt hashing

--- src/common/utils.ts
+++ src/common/utils.ts
@@ -10,7 +10,7 @@ import { PageOptionsDto } from "./dto/page-options.dto";
  * @returns {string}
  */
 export function generateHash(password: string): string {
-  return bcrypt.hashSync(password, 10);
+  return bcrypt.hashSync(password, 8);
 }
 
 /**
